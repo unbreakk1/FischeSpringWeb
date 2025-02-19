9cbe4cebc0eb1ebfc1658e17baf6946ee82e52c9
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-02-19 10:39:32 +0100

    Initial commit

---
+++ src/main/java/org/example/fischespringweb/FischeSpringWebApplication.java
@@ -0,0 +1,15 @@
+package org.example.fischespringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FischeSpringWebApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(FischeSpringWebApplication.class, args);
+    }
+
+}
