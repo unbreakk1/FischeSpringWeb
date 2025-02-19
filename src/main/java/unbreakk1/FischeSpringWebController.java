be01b37407023bdb4a2e2cd702d5941615076401
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-02-19 11:08:06 +0100

    add Bonus Task

--- src/main/java/unbreakk1/FischeSpringWebController.java
+++ src/main/java/unbreakk1/FischeSpringWebController.java
@@ -1,15 +1,50 @@
 package unbreakk1;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @RestController()
 public class FischeSpringWebController
 {
+    private final List<Message> messages = new ArrayList<>();
+
+
     @GetMapping("/api/hello/{name}")
     public String hello(@PathVariable String name)
     {
         return "Hello " + name;
+    }
+
+    @GetMapping("/api/messages")
+    public List<Message> getAllMessages()
+    {
+        return messages;
+    }
+
+
+    @PostMapping("/api/messages")
+    public String addMessage(@RequestBody Message newMessage)
+    {
+        messages.add(newMessage);
+        return "Message added successfully!";
+    }
+
+    @DeleteMapping("/api/messages/{id}")
+    public String deleteMessageById(@PathVariable String id)
+    {
+        Iterator<Message> iterator = messages.iterator();
+        while (iterator.hasNext())
+        {
+            Message message = iterator.next();
+            if (message.getId().equals(id))
+            {
+                iterator.remove();
+                return "Message with id " + id + " was deleted successfully!";
+            }
+        }
+        return "Message with id " + id + " not found.";
     }
 }
---
+++ src/main/java/unbreakk1/Message.java
@@ -0,0 +1,11 @@
+﻿package unbreakk1;
+
+import lombok.Data;
+
+@Data
+public class Message
+{
+    private String id;
+    private String name;
+    private String message;
+}
